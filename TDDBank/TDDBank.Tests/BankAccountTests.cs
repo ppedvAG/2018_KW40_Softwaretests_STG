e46abee91c84158def0370b6c90f0ecf16892644
André <andrerrr@outlook.de>
2018-10-01 15:02:04 +0200

    TDD mit Öffungszeitentests und implementierung

---
+++ TDDBank/TDDBank.Tests/BankAccountTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDBank.Tests
+{
+    //Bankkonto
+    //✔- Kontostand abfragen
+    //✔- Betrag einzahlen(nicht Negativ)
+    //✔- Betrag abheben(nicht Negativ)
+    //✔	- Darf nicht unter 0 fallen
+    //✔ Neues Konto hat 0 als Kontostand
+
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void BankAccount_new_account_has_0_as_balance()
+        {
+            var ba = new BankAccount();
+
+            Assert.AreEqual(0m, ba.Balance);
+        }
+
+        [TestMethod]
+        public void BankAccount_can_deposit()
+        {
+            var ba = new BankAccount();
+
+            ba.Deposit(3m);
+            Assert.AreEqual(3m, ba.Balance);
+
+            ba.Deposit(5m);
+            Assert.AreEqual(8m, ba.Balance);
+        }
+
+        [TestMethod]
+        public void BankAccount_deposit_zero_or_negative_value_throws_ArgumentException()
+        {
+            var ba = new BankAccount();
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Deposit(-3));
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Deposit(0));
+        }
+
+        [TestMethod]
+        public void BankAccount_can_withdraw()
+        {
+            var ba = new BankAccount();
+            ba.Deposit(10m);
+
+            ba.Withdraw(3m);
+            Assert.AreEqual(7m, ba.Balance);
+
+            ba.Withdraw(4m);
+            Assert.AreEqual(3m, ba.Balance);
+        }
+
+        [TestMethod]
+        public void BankAccount_withdraw_zero_or_negative_value_throws_ArgumentException()
+        {
+            var ba = new BankAccount();
+            ba.Deposit(10m);
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Withdraw(-3));
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Withdraw(-0));
+            Assert.ThrowsException<ArgumentException>(() => ba.Withdraw(0));
+        }
+
+        [TestMethod]
+        public void BankAccount_withdraw_more_than_balance_throws_InvalidOperationException()
+        {
+            var ba = new BankAccount();
+            ba.Deposit(10m);
+
+
+            Assert.ThrowsException<InvalidOperationException>(() => ba.Withdraw(20m));
+            Assert.ThrowsException<InvalidOperationException>(() => ba.Withdraw(11m));
+
+            ba.Withdraw(10m);
+            Assert.AreEqual(0m, ba.Balance);
+        }
+    }
+}
---
+++ TDDBank/TDDBank.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDBank.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDBank.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30e908ee-152c-43e2-91aa-d87205779bac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
