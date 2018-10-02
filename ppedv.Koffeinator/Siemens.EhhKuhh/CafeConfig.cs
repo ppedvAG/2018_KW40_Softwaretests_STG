38caf822bd2737fdd9656661693f64ef38d3cdf4
André <andrerrr@outlook.de>
2018-10-02 10:14:08 +0200

    Siemens Kaffee Maschine erstellt

---
+++ ppedv.Koffeinator/Siemens.EhhKuhh/CafeConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Siemens.EhhKuhh
+{
+    public class CafeConfig
+    {
+        public long KaffeeInGramm { get; set; }
+        public double MilchInLiter { get; set; }
+        public bool[] Zucker { get; set; } //jedes true = 1mg zucker
+        public string Kakao { get; set; }
+        public DateTime Löffel { get; set; }
+    }
+}
---
+++ ppedv.Koffeinator/Siemens.EhhKuhh/CafeStarter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Siemens.EhhKuhh
+{
+    public class CafeStarter
+    {
+        CafeConfig cc;
+        bool hot = false;
+        public CafeStarter(CafeConfig cc)
+        {
+            this.cc = cc;
+        }
+
+        public void HeatUp()
+        {
+            Thread.Sleep(2000);
+            hot = true;
+        }
+
+        public void CreateCafe()
+        {
+            Console.Beep(800, 2000);
+            hot = false;
+        }
+    }
+}
