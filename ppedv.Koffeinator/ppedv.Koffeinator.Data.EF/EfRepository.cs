dd89fbcd8feccd703a64cf9b6106c96d85fcada6
André <andrerrr@outlook.de>
2018-10-02 15:13:33 +0200

    Entity Framework Repository implementierung

---
+++ ppedv.Koffeinator/ppedv.Koffeinator.Data.EF/EfRepository.cs
@@ -0,0 +1,53 @@
+﻿using ppedv.Koffeinator.Model;
+using ppedv.Koffeinator.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Koffeinator.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        private EfContext con = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Remove(entity);
+
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return con.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return con.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return con.Set<T>();
+        }
+
+        public void Save()
+        {
+            con.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                con.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
